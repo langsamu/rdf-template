369de7a33c5a137981d8a6b322ff3ac9be15e618
Samu Lang <langsamu@hotmail.com>
2023-06-18 20:29:06 +0100

    Style

--- elements/TermLanguage.js
+++ elements/TermLanguage.js
@@ -2,7 +2,7 @@ class TermLanguage extends HTMLElement {
     initialize(graph, context, stack) {
         if (context.language) {
             this.replaceWith(this.ownerDocument.createTextNode(context.language))
-        }else{
+        } else {
             this.remove()
         }
     }
